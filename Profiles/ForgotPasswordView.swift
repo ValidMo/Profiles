ca3efe21f703f9178b58fb33d9de8a79cc0a771d
ValidMo <mohammadivalid@gmail.com>
2024-03-04 11:03:32 +0300

    Some Logic changes

---
+++ Profiles/ForgotPasswordView.swift
@@ -0,0 +1,18 @@
+//
+//  ForgotPasswordView.swift
+//  Profiles
+//
+//  Created by Valid Mohammadi on 4.03.2024.
+//
+
+import SwiftUI
+
+struct ForgotPasswordView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ForgotPasswordView()
+}
